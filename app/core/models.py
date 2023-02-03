39f19b193539ba285dfd92fd76b8193882fcbc83
jwdiller <jwdiller@my.waketech.edu>
2023-02-03 14:30:54 -0500

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -7,21 +7,25 @@ class Orders(models.Model):
     date = models.DateTimeField()
     shortnote = models.CharField(max_length=20)
     note = models.CharField(max_length=200) # May be needed
-    price = models.IntegerField() # While amount * stockPrice would be default, this changes
+    cents = models.IntegerField() # While amount * stockPrice would be default, this changes
 
-    def __str__(self):        return str(self.date) + " " + str(self.stockID) + " " + str(self.shortnote)
-
+    def __str__(self):
+        return str(self.date) + " " + str(self.stockID) + " " + str(self.shortnote)
+    def price(self):
+        return self.cents / 100
 
 class Stock(models.Model):
     productName = models.CharField(max_length=200)
     upc = models.IntegerField() #ID for 'outside world'
-    price = models.IntegerField() #In cents - needs function wrapping. Not using float because of precision
+    cents = models.IntegerField() #In cents - needs function wrapping. Not using float because of precision
     number = models.IntegerField() #As a generic inventory item, integer
     description = models.CharField(max_length=200)
     supplierID = models.IntegerField()
 
     def __str__(self):
         return str(self.upc) + " " + str(self.productName)
+    def price(self):
+        return self.cents/100
 
 
 class Customers(models.Model):
