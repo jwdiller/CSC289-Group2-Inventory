cc5b8d410f562f11b81dd36de60209b8c857ec9f
jwdiller <jwdiller@my.waketech.edu>
2023-02-03 14:25:46 -0500

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 class Orders(models.Model):
     userId = models.IntegerField()
     customerId = models.IntegerField()
@@ -9,35 +8,36 @@ class Orders(models.Model):
     shortnote = models.CharField(max_length=20)
     note = models.CharField(max_length=200) # May be needed
     price = models.IntegerField() # While amount * stockPrice would be default, this changes
-    
-    def __str__(self):
-        return str(self.date) + " " + self.stockID + " " + self.shortnote;
-    
+
+    def __str__(self):        return str(self.date) + " " + str(self.stockID) + " " + str(self.shortnote)
+
+
 class Stock(models.Model):
     productName = models.CharField(max_length=200)
-    upc - models.IntgerField() #ID for 'outside world'
+    upc = models.IntegerField() #ID for 'outside world'
     price = models.IntegerField() #In cents - needs function wrapping. Not using float because of precision
     number = models.IntegerField() #As a generic inventory item, integer
     description = models.CharField(max_length=200)
     supplierID = models.IntegerField()
-    
+
     def __str__(self):
-        return str(self.upc) + " " + self.productName
-    
+        return str(self.upc) + " " + str(self.productName)
+
+
 class Customers(models.Model):
     userName = models.CharField(max_length=200)
-    email = model.CharField(max_length=200)
-    phoneNumber = model.CharField(max_length=50)
-    note = model.CharField(max_length=200)
-    
+    email = models.CharField(max_length=200)
+    phoneNumber = models.CharField(max_length=50)
+    note = models.CharField(max_length=200)
+
     def __str__(self):
-        return self.userName
-    
-class Suppliers(models.Model):
+        return self.userNameclass
+
+Suppliers(models.Model):
     name = models.CharField(max_length=200)
-    email = model.CharField(max_length=200)
-    phoneNumber = model.CharField(max_length=50)
-    note = model.CharField(max_length=200)
-    
+    email = models.CharField(max_length=200)
+    phoneNumber = models.CharField(max_length=50)
+    note = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
