359e1d686496f859570787e80ab3ec63d0ad996d
jwdiller <jwdiller@my.waketech.edu>
2023-02-07 14:12:35 -0500

    Update views.py

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-import random
+import random # for the about page, can delete later
+from .models import * # All models for the databases
 
 # Create your views here.
 
@@ -7,3 +8,7 @@ def about(request):
 	firstpart = random.choice(['Grand', 'Evil', 'Uncouth', 'Mighty'])
 	secondpart = random.choice(['Lord', 'Bandit', 'Master'])
 	return render(request, 'about.html', {'name': firstpart + " " + secondpart})
+
+
+def customer(request):
+    return render(request, 'customer-db.html', { 'tableitems': Customers.objects.all})
