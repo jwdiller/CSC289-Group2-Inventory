ff77cb1d64fe4793a60e8c62e6a1278dc51c7de1
jwdiller <jwdiller@my.waketech.edu>
2023-02-07 14:11:54 -0500

    Update admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Customers)
+admin.site.register(Suppliers)
+admin.site.register(Stock)
+admin.site.register(Orders)
