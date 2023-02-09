e7da67a96f238ef51643cc1ec4c752fe579efd92
anikethbabu <anikethbabu@gmail.com>
2023-02-09 12:01:00 -0500

    added user creation form

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core import views
-
+from users.views import register
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('register/', register, name='register'),
 ]
