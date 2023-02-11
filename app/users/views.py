36610c1eba2d1e01030684a3f7b8954514c28462
jwdiller <jwdiller@my.waketech.edu>
2023-02-11 11:34:44 -0500

    Add files via upload
    23-02-11 Update Jwdiller (added message to successful user creation or if there is error)

--- app/users/views.py
+++ app/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def register(request):
@@ -8,9 +9,11 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request,('!!Sign Up Successful!!'))
             return redirect('home')
+        else:
+            messages.error(request, ('Error in creating user'))
+            return(request, 'users/register.html', {'form' : form})
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
-
-
