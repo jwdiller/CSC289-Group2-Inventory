f01b2d3bd84228890517ae8e2d23abee12b2ca20
jwdiller <jwdiller@my.waketech.edu>
2023-02-13 13:56:11 -0500

    Update urls.py

--- app/core/urls.py
+++ app/core/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
 	path('about/', views.about, name='about'), #about us, experimental page
 	path('', views.home, name='home'), #home page
  	path('database/customer', views.customer, name='customer'), # Start of database URLS
-  	path('signup', views.customersignup, name='signup'), # Sign up\
-    path('create/supplier', views.suppliersignup, name='suppliersignup'),
-    path('create/stock', views.stocksignup, name='stocksignup'),
-    path('create/order', views.ordersignup, name='ordersignup'),
-    path('create/incoming', views.incomingsignup, name='incomingsignup'),
+	
+  	path('signup', views.customersignup, name='signup'), # Sign up
+	path('create/', views.signuphome, name ='signup-home'),
+	path('create/supplier', views.suppliersignup, name='suppliersignup'),
+	path('create/stock', views.stocksignup, name='stocksignup'),
+	path('create/order', views.ordersignup, name='ordersignup'),
+	path('create/incoming', views.incomingsignup, name='incomingsignup'),
 ]
