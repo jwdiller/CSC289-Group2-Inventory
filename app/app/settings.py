4d871d07a2f7b723ef122aa68f3f0767395cd972
Aniketh Babu <anikethbabu@gmail.com>
2023-03-28 11:23:25 -0400

    added profile

--- app/app/settings.py
+++ app/app/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'users/templates'],
+        'DIRS': [BASE_DIR / 'users/templates', BASE_DIR / 'core/templates', BASE_DIR / 'core/templates/core', BASE_DIR / 'users/templates/users'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,6 +142,9 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- app/core/urls.py
+++ app/core/urls.py
@@ -3,11 +3,14 @@ from .forms import *
 from . import views, query
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from users.views import *
 # These represent the url locations for each page
 urlpatterns = [
   path('', views.home, name='home'), #home page
 	path('about/', views.about, name='about'), #about us, experimental page
- 
+  
+  # Start of database URLS
  	path('database/customer', views.customer, name='customerDB'), # Start of database URLS
   path('database/supplier', views.supplier, name='supplierDB'), # Add to
   path('database/stock', views.stock, name='stockDB'),          # core/templates/database.html
@@ -15,6 +18,7 @@ urlpatterns = [
   path('database/incoming', views.incoming, name='incomingDB'),
   path('database/', views.dbhome, name='DB-home'), # Also this line to the general navbar base.html
   
+  # Start of create URLS
   path('create/customer', views.customersignup, name='customersignup'), # Sign up
   path('create/supplier', views.suppliersignup, name='suppliersignup'), # Add to
   path('create/stock', views.stocksignup, name='stocksignup'),          # core/templates/core/create-entry.html
@@ -30,5 +34,13 @@ urlpatterns = [
   #Custom Queries
   path('chart/sold/month/<int:month>/id/<int:id>', query.numbersold, name='numsold'),
   path('chart/profit/month/<int:month>/id/<int:id>', query.profit, name='profit'),
+
+  #profile
+  path('profile/', profile , name='profile'),
+  #password reset
+  path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+  path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+  path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+  path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'), 
 #path('top_5_stocks/', views.top_5_stocks, name='top_5_stocks'), # Top 5 Stocks (Query)
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
