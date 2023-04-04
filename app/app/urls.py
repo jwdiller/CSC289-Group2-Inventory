649d6116685e2fc37ad0ea45db75adb9ea0df907
jwdiller <jwdiller@my.waketech.edu>
2023-04-04 14:29:55 -0400

    Update urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('cashier/', include('cashier.urls')),
+    
     path('register/', register, name='register'),
     path('login/',  auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
